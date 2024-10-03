6fbb32205a84f9a00468d60967c854f5405120c2
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-02 22:11:55 -0500

    Add border box to whole document

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function createGrid(gridSize) {
     
     gridContainer = document.querySelector(".grid-container")
 
-    for (let i =0 ; i < gridSize; i++) {
+    for (let i = 0 ; i < gridSize; i++) {
         const column = document.createElement('div');
         column.className = 'square';
         gridContainer.appendChild(column);
 
-        for (let j = 0; j < gridSize; j++) { 
+        for (let j = 1; j <= gridSize; j++) { 
             // j = 1; j <= gridSize
             const row = document.createElement('div');
             row.className = 'square';
