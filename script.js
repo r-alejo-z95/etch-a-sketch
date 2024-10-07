5ec830e21990c3665246c8009f69b8dbb24f3d01
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-06 21:31:13 -0500

    Refactor createGrid function

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+const gridSide = 500;
+let rows = 5;
+let cols = 5;
+let gridSize = rows * cols;
 
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.width = `${gridSide}px`;
+gridContainer.style.height = `${gridSide}px`;
 
-
-function createGrid(gridSide) {
+function createGrid() {
     
     grid = document.querySelector(".grid")
 
-    for (let i = 0 ; i < (gridSide * gridSide); i++) {
+    for (let i = 0 ; i < gridSide; i++) {
         let square = document.createElement('div');
 
         square.className = 'square';
-        square.style.width = `calc(100% / ${gridSide})`;
-        square.style.height = `calc(100% / ${gridSide})`;
+        square.style.width = `${(gridSide / cols) - 2}px`;
+        square.style.height = `${(gridSide / rows) - 2}px`;
         grid.appendChild(square);
     }
 }
 
-createGrid(5);
+createGrid();
 
