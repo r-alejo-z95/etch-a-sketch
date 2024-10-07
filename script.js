f6052c31fe22dbc2c22e5d60506333a28b5b50cc
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-06 21:08:21 -0500

    Clean createGrid function

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 
-function createGrid(gridSize) {
+
+
+function createGrid(gridSide) {
     
     grid = document.querySelector(".grid")
 
-    for (let i = 0 ; i < gridSize; i++) {
-        let column = document.createElement('div');
-        column.textContent = '';
-        column.className = 'column';
+    for (let i = 0 ; i < (gridSide * gridSide); i++) {
+        let square = document.createElement('div');
 
-        for (let j = 1; j <= gridSize; j++) { 
-            let row = document.createElement('div');
-            row.className = 'row';
-            row.textContent = ('');
-            row.style.flexBasis = `calc(100% / ${gridSize})`;
-            row.style.height = `calc(100% / ${gridSize})`;
-            column.appendChild(row);
-        }
-        grid.appendChild(column);
+        square.className = 'square';
+        square.style.width = `calc(100% / ${gridSide})`;
+        square.style.height = `calc(100% / ${gridSide})`;
+        grid.appendChild(square);
     }
 }
 
