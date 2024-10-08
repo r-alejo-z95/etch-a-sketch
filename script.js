722c0edb798f8dea86a484291f9c5dfa8fd18fce
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-07 22:20:08 -0500

    Add user input funcionality to get grid size

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const gridSide = 500;
-let rows = 5;
-let cols = 5;
+let rows = 16;
+let cols = rows;
 let gridSize = rows * cols;
 
-const gridContainer = document.querySelector('.grid-container');
+let sizeInput = document.querySelector(".select-size");
+sizeInput.addEventListener("input", function () {
+  rows = parseInt(sizeInput.value);
+  cols = rows;
+  console.log(`Nuevo tamaño de grid: ${rows}x${cols}`);
+});
+
+const gridContainer = document.querySelector(".grid-container");
 gridContainer.style.width = `${gridSide}px`;
 gridContainer.style.height = `${gridSide}px`;
 
 function createGrid() {
+  for (let i = 0; i < gridSize; i++) {
+    let square = document.createElement("div");
 
-    for (let i = 0 ; i < gridSize; i++) {
-        let square = document.createElement('div');
-
-        square.className = 'square';
-        square.style.width = `${(gridSide / cols)}px`;
-        square.style.height = `${(gridSide / rows)}px`;
-        gridContainer.appendChild(square);
-    }
+    square.className = "square";
+    square.style.width = `${gridSide / cols}px`;
+    square.style.height = `${gridSide / rows}px`;
+    gridContainer.appendChild(square);
+  }
 }
 
 createGrid();
-
