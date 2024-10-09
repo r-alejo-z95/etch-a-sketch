62dc67cfa1bbd8c49e53442efcaad25fd68f3da8
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-09 00:03:08 -0500

    Create array of colors to select through getRandomColor ()

--- script.js
+++ script.js
@@ -39,6 +39,25 @@ sizeInput.addEventListener("change", () => {
   createGrid();
 });
 
+const colorsArray = [
+  "rgba(255, 0, 0, 1) 0%",
+  "rgba(255, 154, 0, 1) 10%",
+  "rgba(208, 222, 33, 1) 20%",
+  "rgba(79, 220, 74, 1) 30%",
+  "rgba(63, 218, 216, 1) 40%",
+  "rgba(47, 201, 226, 1) 50%",
+  "rgba(28, 127, 238, 1) 60%",
+  "rgba(95, 21, 242, 1) 70%",
+  "rgba(186, 12, 248, 1) 80%",
+  "rgba(251, 7, 217, 1) 90%",
+  "rgba(255, 0, 0, 1) 100%",
+];
+
+function getRandomColor(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  return (randomColor = arr[randomIndex]);
+}
+
 let isGradient = false;
 
 const rainbowBtn = document.querySelector(".rainbow");
@@ -46,7 +65,7 @@ rainbowBtn.addEventListener("click", () => {
   squares.forEach((square) => {
     if (!isGradient) {
       square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "blue";
+        square.style.backgroundColor = getRandomColor(colorsArray);
       });
     } else {
       square.addEventListener("mouseover", () => {
