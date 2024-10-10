ea0c266f6e129469e6f800ef8f44e5cef5dde92e
Ramon <170786939+r-alejo-z95@users.noreply.github.com>
2024-10-09 23:58:06 -0500

    Update rainbowBtn event listener to paint random colors properly

--- script.js
+++ script.js
@@ -23,12 +23,6 @@ function createGrid() {
     square.style.width = square.style.height;
     gridContainer.appendChild(square);
   }
-  squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = currentColor;
-    });
-  });
 }
 
 const sizeInput = document.querySelector(".select-size");
@@ -63,16 +57,25 @@ function getRandomColor() {
 }
 
 let isGradient = false;
-let currentColor = "black";
 
 const rainbowBtn = document.querySelector(".rainbow");
 rainbowBtn.addEventListener("click", () => {
   if (!isGradient) {
-    currentColor = getRandomColor();
+    squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = getRandomColor();
+      });
+    });
     rainbowBtn.style.background =
       "linear-gradient(90deg, rgba(255, 0, 0, 1) 0%, rgba(255, 154, 0, 1) 10%, rgba(208, 222, 33, 1) 20%, rgba(79, 220, 74, 1) 30%, rgba(63, 218, 216, 1) 40%, rgba(47, 201, 226, 1) 50%, rgba(28, 127, 238, 1) 60%, rgba(95, 21, 242, 1) 70%, rgba(186, 12, 248, 1) 80%, rgba(251, 7, 217, 1) 90%, rgba(255, 0, 0, 1) 100%)";
   } else {
-    currentColor = "black";
+    squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+      });
+    });
     rainbowBtn.style.background = "";
   }
   isGradient = !isGradient;
